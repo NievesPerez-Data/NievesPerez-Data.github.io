9322a36e731fc74aedd24a00f62f80eb9939da81
NievesPerez-Data <mnieves.perez.alonso@gmail.com>
2026-02-06 18:53:01 +0100

    Change output to static and remove Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
   site: 'https://NievesPerez-Data.github.io',
   base: '/NievesPerez-Data',
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: "static,
 });
