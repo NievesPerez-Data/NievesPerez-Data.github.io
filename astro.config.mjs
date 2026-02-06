933a8fd1cabe2b5ed5c56315264f2028481f5df8
NievesPerez-Data <mnieves.perez.alonso@gmail.com>
2026-02-06 18:55:49 +0100

    Update base URL and output format in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-// If you are not going to use vercel analytics you can delete the analytics
-// If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
   site: 'https://NievesPerez-Data.github.io',
-  base: '/NievesPerez-Data',
-  output: "static,
+  base: '/NievesPerez-Data.github.io',
+  output: 'static'
 });
