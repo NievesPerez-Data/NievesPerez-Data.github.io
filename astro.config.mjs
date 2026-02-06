e61f684553ebf7d52b574e99642a7be4a3bd6797
NievesPerez-Data <mnieves.perez.alonso@gmail.com>
2026-02-06 18:40:42 +0100

    Modify site and base configuration for Astro
    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 // If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
   site: 'https://NievesPerez-Data.github.io',
+  base: '/NievesPerez-Data',
   output: "server",
   adapter: vercel({
     webAnalytics: {
@@ -12,7 +13,3 @@ export default defineConfig({
     },
   }),
 });
-
-//If you don't have vercel the configuration will be empty
-
-//ej : export default defineConfig({});
